a6070928f5d6d03cc4b040bdb3bc14893dceeb38
BastianPrado <bastian.prado.v@gmail.com>
2020-07-28 22:40:07 -0700

    Added some images so that is easier to access them

--- Motor.py
+++ Motor.py
@@ -1,6 +1,24 @@
+import pygame
+
 class motor:
+    # Class Motor constructor
     def __init__(self):
         print("Initializing game")
 
+    # When this function is called in the main, the game intializes
     def run(self):
         print("run")
+        # INITIATES PYGAME
+        pygame.init()
+
+        #RGB COLORS
+        white = [255, 255, 255]
+        red = [255, 0, 0]
+        green = [0, 255, 0]
+        blue = [0, 0, 255]
+        black = [0, 0, 0]
+        paleLavander = [213, 204, 255]
+        lightRed = [255, 51, 51]
+        lightGray = [224, 224, 223]
+        yellow = [215, 209, 43]
+        lightBlue = [173, 216, 230]
